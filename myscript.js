67710cc03be8f1add6acb73a20b593a7cf9555b8
vanessatian812 <sasa.tian2020@gmail.com>
2025-01-09 10:14:48 -0800

    Add files via upload

--- myscript.js
+++ myscript.js
@@ -1,21 +1,33 @@
-
 window.onload = function(){
+
+  questionElem = document.getElementById("question");
+  answerElem = document.getElementById("answers");
     fetchQuestions();
 }
 
 async function fetchQuestions(){
 
-    //let url = "https://opentdb.com/api.php?amount=1";
-    let url = src="questions.json";
+    let url = "https://opentdb.com/api.php?amount=10";
+    //let url = "questions.json";
 
     try {
         const response = await fetch(url);
         const data = await response.json();
 
         console.log(data);
+        
+        changeContent(data);
 
       } catch (error) {
         console.error('Error fetching activity:', error);
       }
+}
+
+function changeContent(data){
+
+  questionElem.innerHTML = `${data[0].question}`;
+
+  answerElem.innerHTML = `${data[0].correct_answer}` + `${data[0].incorrect_answers}`;
+
 }
 
