e856db03d5aeca8166f02265d5a5707c45ce735d
vanessatian812 <sasa.tian2020@gmail.com>
2025-01-21 10:34:12 -0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+const version = 'v3';  // change this everytime you update the service worker
+                          // to force the browser to also update it.
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('my-cache').then(function(cache) {
+      return cache.addAll([
+        'index.html',
+        'styles.css',
+        'myscript.js'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// load the service worker
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('sw.js').then(function(registration) {
+        console.log('Service Worker registered with scope:', registration.scope);
+      }, function(error) {
+        console.log('Service Worker registration failed:', error);
+      });
+    });
+  }                  
